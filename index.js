a9ab44483fcb2c52621f73ee46ea54fe09cf58ca
Evan hameed <evanhameed9966@gmail.com>
2020-06-10 04:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const tutorials = [
     'what is JSONP?'
   ];
 
-  const titleCased = () => tutorials.map(string => string.split(' ').map(word => word.toLowerCase().charAt(0).toUpperCase() + word.slice(1)).join(' '))
+  const titleCased = () => tutorials.map(string => string.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
